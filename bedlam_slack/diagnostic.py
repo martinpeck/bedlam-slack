2a223ddd6831f8cbeda166868b19404988c48762
Martin Peck <mpeck@hotmail.co.uk>
2016-04-17 08:25:19 +0100

    remove comment

--- bedlam_slack/diagnostic.py
+++ bedlam_slack/diagnostic.py
@@ -2,7 +2,6 @@ from bedlam_slack import app
 import os
 from flask import jsonify, request
 
-# for testing that environment variables are being picked up
 @app.route("/test/env", methods=["GET"])
 def test_environment():
     return os.environ["TEST_ENV_VALUE"]
