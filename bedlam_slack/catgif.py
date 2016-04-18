892541eb7dc178efef0f26e8e9af42a1ec0f89ef
Martin Peck <mpeck@hotmail.co.uk>
2016-04-18 23:56:05 +0100

    Fix bug with duplicate cats and add tests. Closes #5. Closes #11

--- bedlam_slack/catgif.py
+++ bedlam_slack/catgif.py
@@ -1,12 +1,30 @@
 from bedlam_slack import app, slack_helper
 from flask import jsonify
+import requests
+from bs4 import BeautifulSoup
+
+CAT_API_URL = "http://thecatapi.com/api/images/get?format=xml&type=gif&results_per_page=1"
+CAT_API_TOKEN = ""
+
+# extract the url from the response text
+def parse_cat_api_response(response_text):    
+    soup = BeautifulSoup(response_text, "lxml")
+    return soup.image.url.text
 
 # returns a random cat gif from thecatapi.com
+def get_cat_image(token, url):
+    resp = requests.get(url)
+    if resp.status_code == 200:       
+        return parse_cat_api_response(resp.text)
+        
 @app.route("/slash/cat-gif", methods=['POST'])
 def random_cat_gif():
     if not slack_helper.validate_request():
         abort(403)
     
+    image_url = get_cat_image(CAT_API_TOKEN, CAT_API_URL)
+     
+    # return response
     response = {
         "response_type": "in_channel",
         "text": "Here is your random cat:",
@@ -14,7 +32,7 @@ def random_cat_gif():
             {
                 "fallback" : "A cat gif",
                 "title" : "Random cat gif",
-                "image_url" : "http://thecatapi.com/api/images/get?format=src&type=gif"
+                "image_url" : image_url
             }
           ]
         }
---
+++ tests/catgif_tests.py
@@ -0,0 +1,50 @@
+from nose.tools import *
+import bedlam_slack
+import json
+import responses
+
+class CatGit_Tests:
+            
+    def setup(self):
+        self.app = bedlam_slack.app.test_client()
+        self.response_text = """<?xml version="1.0"?>
+            <response>
+            <data>
+                <images>
+                <image>
+                    <url>http://foo.bar.baz/123/456</url>
+                    <id>test</id>
+                    <source_url>http://thecatapi.com/?id=test</source_url>
+                </image>
+                </images>
+            </data>
+            </response>
+        """
+
+    def test_parse_cat_api_response(self):
+    
+        url = bedlam_slack.catgif.parse_cat_api_response(self.response_text)
+        assert url == "http://foo.bar.baz/123/456"       
+   
+    @responses.activate
+    def test_cat_gif_requests_are_working(self):
+    
+        # set up mock response from API
+        responses.add(responses.GET, "http://thecatapi.com/api/images/get",
+              body=self.response_text, status=200,
+              content_type='application/json')
+
+        # act      
+        response = self.app.post("/slash/cat-gif")
+
+        # assert    
+        assert response.status_code == 200       
+        data = json.loads(response.get_data(as_text=True))
+
+        assert data.get("text") is not None
+        assert data["text"] ==  "Here is your random cat:"
+        
+        assert data.get("attachments") is not None
+        assert len(data["attachments"]) == 1
+        assert data["attachments"][0].get("image_url") is not None
+        assert data["attachments"][0]["image_url"] == "http://foo.bar.baz/123/456"
